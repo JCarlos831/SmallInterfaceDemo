1d814ea5603fbcfa2ba9a9f50aa7f00a87b03a05
Juan Montoya <jcmontoya@gmail.com>
2018-08-21 15:45:02 -0400

    Interface Demo from C Sharp Corner

---
+++ InterfaceDemo/ODDEVEN.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    interface IOne
+    {
+        void ONE(); // Pure Abstract Method Signature
+    }
+
+    interface ITwo
+    {
+        void TWO();
+    }
+
+    interface IThree : IOne
+    {
+        void THREE();
+    }
+
+    interface IFour
+    {
+        void FOUR();
+    }
+
+    interface IFive : IThree
+    {
+        void FIVE();
+    }
+
+    interface IEVEN : ITwo, IFour { }
+
+    class ODDEVEN: IEVEN, IFive // Must Implement all the abstract method, in Derived class
+    {
+        public void ONE() // Implementation of the Abstract Method
+        {
+            Console.WriteLine("This is ONE");
+        }
+
+        public void TWO() // Implementation of the Abstract Method
+        {
+            Console.WriteLine("This is TWO");
+        }
+
+        public void THREE() // Implementation of the Abstract Method
+        {
+            Console.WriteLine("This is THREE");
+        }
+
+        public void FOUR() // Implementation of the Abstract Method
+        {
+            Console.WriteLine("This is FOUR");
+        }
+
+        public void FIVE() // Implementation of the Abstract Method
+        {
+            Console.WriteLine("This is FIVE");
+        }
+    }
+
+}
